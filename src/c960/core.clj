7c0155fbb589862cdee327760b124be7eae9a86d
Josh Reighley <jreighley@gmail.com>
2016-09-03 00:25:18 -0700

    Total re-write.

--- src/c960/core.clj
+++ src/c960/core.clj
@@ -1,38 +1,23 @@
 (ns  c960.core)
 
-(defn buildc960 []
- ; Build a deck of cards 1 - 8 and shuffle them
-(def fulldeck(shuffle(seq(range 1 9 ))))
-;First even and first odd card get bishops
-(def B1(first (filter even? fulldeck)))
-(def B2 (first (filter odd? fulldeck)))
-;remove the bishop cards and take the next 3 for Queens and knights
-(def Q1 (first (remove (set[B1 B2]) fulldeck)))
-(def N1 (nth (remove (set[B1 B2]) fulldeck) 1))
-(def N2 (nth (remove (set[B1 B2]) fulldeck) 2))
-;remove the cards used by the bishops knights and queens and sort the last 3
-;place the Rooks on the outside and the king in the middle of the remaining cards
-(def R1 (first(sort(remove (set[B1 B2 N1 N2 Q1]) fulldeck) )))
-  (def K1 (nth(sort(remove (set[B1 B2 N1 N2 Q1]) fulldeck) )1))
-  (def R2 (nth(sort(remove (set[B1 B2 N1 N2 Q1]) fulldeck) )2))
-;print the piece names from 1 to 8
-(def pieceseq(for [nxtsquare (range 1 9)]
-  (cond
-   (= Q1 nxtsquare) "Q"
-   (= B1 nxtsquare) "B"
-   (= B2 nxtsquare) "B"
-   (= N1 nxtsquare) "N"
-   (= N2 nxtsquare) "N"
-   (= R1 nxtsquare) "R"
-   (= R2 nxtsquare) "R"
-   (= K1 nxtsquare) "K"
-   )))
-  (println pieceseq)
-  (println "type y for another")
-)
+(def pclist '(B B N N Q R K R))
 
+(defn c960 [] "returns a quasi-random 8 number sequence insuring that the first item is even,the second item is odd and the last 3 items are sorted ascending"
+              (let [c960seq (shuffle (range 1 9))
+                    oddBish (first (filter odd? c960seq))
+                    evenBish (first (filter even? c960seq))
+                    goodseq  (->> c960seq
+                              (filter #(not= evenBish %))
+                              (filter #(not= oddBish %)))]
+                (flatten (conj [evenBish oddBish]
+                               (take 3 goodseq)
+                               (sort (take-last 3 goodseq))))))
+
+(defn buildc960 [] "correlates positions for chess peices to the generated list"
+                (->> pclist
+                   (zipmap (c960))
+                   (sort)
+                   (vals)))
+                   
 (defn -main []
-  (buildc960)
-  (while (==  0 (compare "y" (read-line)))
-    (buildc960))
-  )
+  (println (buildc960)))
